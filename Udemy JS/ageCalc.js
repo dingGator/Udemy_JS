97f21a70b5db2aa196a2b31fe2c3b0904ddff225
dingGator <kim@chucrew.net>
2017-06-11 10:48:43 -0400

    Udemy JS
    Udemy JS

---
+++ Udemy JS/ageCalc.js
@@ -0,0 +1,3 @@
+var userAge = prompt("How old are you?");
+var dayAlive = userAge*365.25;
+alert(userAge + " years is roughly "+  dayAlive+ " days");
---
+++ Udemy JS/script.js
@@ -0,0 +1,4 @@
+//alert("Hello from JS Files!");
+var userName = prompt("what is your name?");
+alert("Nice to meet you, "+ userName);
+console.log("Also great to meet you, "+ userName);
