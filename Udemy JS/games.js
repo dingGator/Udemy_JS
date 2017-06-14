c0131bab63238c53a9eea7681732eea4d6284b27
dingGator <kim@chucrew.net>
2017-06-13 21:23:30 -0400

    Udemy JS: Games
    Udemy JS Games.js

--- Udemy JS/games.js
+++ Udemy JS/games.js
@@ -1,5 +1,20 @@
 //create secretNumber
-var secretNumber =4;
+var secretNumber = 4;
 //ask user for guess
-var guess = prompt("Guess a number");
+var stringGuess = prompt("Guess a number");
+var guess = Number(stringGuess);
 alert(guess);
+//check if guess is right
+if(guess === secretNumber){
+	alert("You got it right.")
+}
+//check if number is higher
+else if (guess > secretNumber){
+	alert("too high.  guess again")
+}
+//check if number is lower
+
+else{
+
+	alert("too low. guess again!")
+}
