928e9460b08d893f4a27a389697246f517b9d190
dingGator <kim@chucrew.net>
2017-06-18 21:40:49 -0400

    JS function
    JS function

---
+++ Udemy JS/functionEx.js
@@ -0,0 +1,25 @@
+//function isEven(num1){
+	// return true if even
+	//return false otherwise
+//
+//	if (num1 %2 ===0){
+//		return true;
+//	} else{
+//		return false;
+//	}
+//  }
+function isEven(num1) {
+	return num1 %2===0;
+	// body...
+}
+
+function factorial(num2){
+	//define a result variable
+	var result =1;
+	//calculate factorial and store value in result
+	for(var i =2;i<=num; i++){
+		result *=i;
+	}
+	//return the result variable
+	return result;
+}
