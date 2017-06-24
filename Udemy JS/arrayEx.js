2fc0c4b0e91f362374613acd56106f1e864bb213
dingGator <kim@chucrew.net>
2017-06-24 12:24:30 -0400

    JS Array
    JS Array

--- Udemy JS/arrayEx.js
+++ Udemy JS/arrayEx.js
@@ -10,7 +10,10 @@ function printReverse(arrayPrint){
 		console.log(arrayPrint[i]);
 	}
 }
+printReverse([3,6,2,5]);
 //	<li>"isUniform()" - Takes an array as an argument and returns true if all elements in the array are identical</li>
+
+
 
 function isUniform(arrayUni){
 	var first = arrayUni[0];
@@ -18,24 +21,27 @@ function isUniform(arrayUni){
 	for(var i =1; i< arrayUni.length; i++){
 
 	if (arrayUni[i] !== first) {
+		console.log("arrayUni[i] !==first "+ arrayUni[i])
 		return false;
-	}}
-	 return true;
-
+	}}return true;
 }
+console.log("isUniform([6,6,6,6,6,6]) " + isUniform([6,6,6,6,6,6]));
+console.log("isUniform([2,5,2,2,3,2,2]) "+ isUniform([2,5,2,2,3,2,2]));
 //	<li>"sumArray()" - Accepts an array of numbers and returns the sum of all numbers in the array</li>
 //var numSumArray=[3,6,2,5,8,10];
 
 
 
 
-function sumAr(arr){
-var sumArray= 0;
+function sumArray(arr){
+var total= 0;
 	arr.forEach(function(element){
-		sumArray += element;
-	}
+		total += element;
+	});
 	return total;
+
 }
+console.log("sumArray(3,6,2,5,8,10) "+ sumArray([3,6,2,5,8,10]));
 //	<li>"max()" - Accepts an array of numbers and returns the maximum number in the array</li>
 //var numMaxArray = [2,9,8,7,2,6,5];
 
@@ -45,8 +51,11 @@ function maxNum(arr){
 	for(var i=1;i<arr.length;i++){
 		if(arr[i]>maxNum){
 			maxNum= arr[i];
-		}else{
-			numMaxArray[i+1]=  maxNum;
 		}
+		} return maxNum;
 	}
-}
+
+
+
+
+console.log("maxNum([2,8,7,2,6,5] "+ maxNum([2,8,7,2,6,5]));
