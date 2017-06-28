9d6908f9896434c9ec010b6f61a787218b8595de
dingGator <kim@chucrew.net>
2017-06-27 22:02:08 -0400

    JS methods
    JS methods

---
+++ Udemy JS/methods.js
@@ -0,0 +1,37 @@
+var obj ={
+	name:"Chuck",
+	age: 45,
+	isCool: false,
+	friends:["bob", "tina"],
+	add:function(x,y){
+		return x+y;
+	}
+}
+
+console.log("obj.add(10,5)=  " + obj.add(10,5));
+
+var dogSpace ={};
+dogSpace.speak = function(){
+	return "WOOF!";
+}
+var catSpace ={};
+catSpace.speak = function(){
+	return "MEOW!";
+}
+
+console.log("dogSpace.speak = " + dogSpace.speak());
+console.log("catSpace.speak = " + catSpace.speak());
+
+var comments ={};
+comments.data =	["good job",
+				 "bye",
+				 "lame"];
+
+
+comments.print= function(){
+	this.data.forEach(function(el){
+		console.log(el);
+	});
+}
+
+
