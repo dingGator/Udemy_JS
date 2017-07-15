8151d74b8fa49e1cfb25c5db4e4415da82a14a50
dingGator <kim@chucrew.net>
2017-07-14 23:38:07 -0400

    JS Color Game
    JS Color Game

---
+++ Udemy JS/colorGame.js
@@ -0,0 +1,22 @@
+var path= document.scripts[document.scripts.length -1].src;
+var fileName = path.substring(path.lastIndexOf('/')+1);
+function clearTop(){
+
+	console.clear();
+console.log( " JS  filename  Connected:  "+ fileName );
+}
+clearTop();
+
+var colors =[
+"rgb(255, 0, 0)",
+"rgb(255, 255, 0)",
+"rgb(0, 255, 0)",
+"rgb(0, 255, 255)",
+"rgb(0, 0, 255)",
+"rgb(255, 0, 255)"
+];
+
+var squares = document.querySelectorAll(".square");
+for(var i=0;i<squares.length;i++){
+	squares[i].style.backgroundColor = colors[i];
+};
