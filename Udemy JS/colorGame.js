de0356ddd257546450327cf372e06703f52a1531
dingGator <kim@chucrew.net>
2017-07-16 23:32:04 -0400

    JS Color Game
    JS Color Game

--- Udemy JS/colorGame.js
+++ Udemy JS/colorGame.js
@@ -24,6 +24,18 @@ var pickedColor = colors[3];
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 =document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+resetButton.addEventListener("click",function(){
+	//generate all new colors
+	colors =generateRandomColors(6);
+	//pick a new random color from array
+	pickedColor = pickColor();
+	//change colorDisplay to match picked Color
+	colorDisplay.textContent = pickedColor;
+	//change colors of squares
+	for(var i=0;i<squares.length;i++){
+	squares[i].style.backgroundColor = colors[i];
+}});
 colorDisplay.textContent = pickedColor;
 for(var i=0;i<squares.length;i++){
 //add initial colors to squares
