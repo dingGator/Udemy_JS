f836af72fafc7d5c0cc178c220da2a9040cf102a
dingGator <kim@chucrew.net>
2017-07-18 20:46:09 -0400

    JS Color Game
    JS Color Game

--- Udemy JS/colorGame.js
+++ Udemy JS/colorGame.js
@@ -17,26 +17,66 @@ var colors =[
 "rgb(255, 0, 255)"
 ];
 */
-
-var colors = generateRandomColors(6);
+var numSquares =6;
+var colors = generateRandomColors(numSquares);
 var squares = document.querySelectorAll(".square");
 var pickedColor = colors[3];
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 =document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+
+easyBtn.addEventListener("click",function(){
+	hardBtn.classList.remove("selected");
+	easyBtn.classList.add("selected");
+	numSquares =3;
+	colors =generateRandomColors(numSquares);
+	pickedColor = pickColor();
+	colorDisplay.textContent= pickedColor;
+	for(var i =0; i<squares.length;i++){
+		if(colors[i]){
+			squares[i].style.backgroundColor = colors[i];
+		}else{
+			squares[i].style.display = "none";
+		}
+	}
+
+});
+
+hardBtn.addEventListener("click",function(){
+	easyBtn.classList.remove("selected");
+	hardBtn.classList.add("selected");
+	numSquares =6;
+	colors =generateRandomColors(numSquares);
+	pickedColor = pickColor();
+	colorDisplay.textContent= pickedColor;
+	for(var i =0; i<squares.length;i++){
+
+			squares[i].style.backgroundColor = colors[i];
+
+			squares[i].style.display = "block";
+		}
+	}
+
+)
+
+
 resetButton.addEventListener("click",function(){
 	//generate all new colors
-	colors =generateRandomColors(6);
+	colors =generateRandomColors(numSquares);
 	//pick a new random color from array
 	pickedColor = pickColor();
 	//change colorDisplay to match picked Color
 	colorDisplay.textContent = pickedColor;
+	this.textContent ="New Colors";
+	messageDisplay.textContent="";
 	//change colors of squares
 	for(var i=0;i<squares.length;i++){
 	squares[i].style.backgroundColor = colors[i];
 }
-h1.style.backgroundColor ="#232323";
+h1.style.backgroundColor ="steelblue";
 });
 colorDisplay.textContent = pickedColor;
 for(var i=0;i<squares.length;i++){
@@ -47,12 +87,12 @@ for(var i=0;i<squares.length;i++){
 		//grab color of clicked square
 		var clickedColor = this.style.backgroundColor;
 		//compare color to pickedColor
-		console.log(clickedColor + "  1  "+ pickedColor);
+		//console.log(clickedColor + "  1  "+ pickedColor);
 		if (clickedColor === pickedColor){
-		console.log(clickedColor + "  2  "+ pickedColor);
+		//console.log(clickedColor + "  2  "+ pickedColor);
 
 			messageDisplay.textContent ="Correct!";
-			resetButton.textContent ="Play Again"
+			resetButton.textContent ="Play Again?"
 			changeColors(clickedColor);
 			h1.style.backgroundColor= clickedColor;
 		}else{
@@ -72,7 +112,7 @@ function changeColors(color){
 
 function pickColor(){
 	var random = Math.floor(Math.random()*colors.length);
-	return color[random];
+	return colors[random];
 }
 
 function generateRandomColors(num){
