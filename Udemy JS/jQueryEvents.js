ae62ce7b1934f97981a47eca25a94cc503650667
dingGator <kim@chucrew.net>
2017-07-26 14:29:05 -0400

    jQueryEvents
    jQuery Events

---
+++ Udemy JS/jQueryEvents.js
@@ -0,0 +1,37 @@
+var path= document.scripts[document.scripts.length -1].src;
+var fileName = path.substring(path.lastIndexOf('/')+1);
+function clearTop(){
+
+	console.clear();
+console.log( " JS  filename  Connected:  "+ fileName );
+}
+clearTop();
+
+
+if(jQuery){
+	console.log("jQuery Loaded");
+}else{
+	console.log("No jQuery :( ");
+};
+
+$("h1").click(function(){
+	console.log("h1 clicked!");
+});
+$("button").click(function(){
+	console.log("button clicked!");
+	var text=$(this).text();
+	$(this).css("background", "pink");
+	console.log("You clicked:  "+ text);
+});
+
+$("input").keypress(function(){
+ console.log("You pressed a key!  ");
+});
+
+$("input").keypress(function(event){
+ console.log("event You pressed a key!  "+ event);
+});
+$("input").keypress(function(event){
+ if(event.which ===13){
+ console.log("you hit enter!  "+ $(event.which.keycode));
+}});
