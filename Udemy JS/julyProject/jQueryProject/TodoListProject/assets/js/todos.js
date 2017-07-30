cc0e105067cfd5f492e66ad6cc87d5430e3956f0
dingGator <kim@chucrew.net>
2017-07-29 21:19:16 -0400

    Todo List Project
    Todo List Project

--- Udemy JS/julyProject/jQueryProject/TodoListProject/assets/js/todos.js
+++ Udemy JS/julyProject/jQueryProject/TodoListProject/assets/js/todos.js
@@ -13,3 +13,72 @@ if(jQuery){
 }else{
 	console.log("No jQuery :( ");
 };
+
+
+//event listener to existing and future li
+$("ul").on("click","li",function(){
+	$(this).toggleClass("completed");
+	console.log("li clicked toggleClass");
+
+});
+$("ul").on("click","span",function(event){
+	console.log("span clicked");
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+	console.log("keypress");
+	if(event.which === 13){
+		console.log("you hit enter");
+		//grab new todo text from input
+		var todoText = $(this).val();
+		$(this).val("");
+		//create a new li and add to ul
+		$("ul").append("<li><span>X</span> "+todoText+"</li>");
+	}
+
+});
+/*/show event bubbling effect
+$("ul").click(function(){
+	console.log("ul clicked");
+});
+$("#container").click(function(){
+	console.log("container div clicked");
+});
+$("body").click(function(){
+	console.log("body clicked");
+})
+/*change color of list item
+//cross off item
+$("li").click(function(){
+	console.log("clicked li");
+	//if li is gray
+	if($(this).css("color")==="rgb(128, 128, 128)"){
+		console.log("It is currently gray!")
+		//turn it black
+		$(this).css({
+		color: "black",
+		textDecoration: "none"
+		});
+		console.log("no line through");
+		console.log("color black");
+
+	}else{
+		//turn it gray
+	$(this).css({
+		color: "gray",
+		textDecoration: "line-through"
+	});
+	console.log("line through");
+	console.log("color gray");
+
+
+	}
+
+
+
+});
+*/
